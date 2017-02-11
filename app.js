3c102150fa81b3179851ba271604668bc74461f0
mikieseven <miguel@xopolus.com>
2017-02-10 22:57:38 -0600

    data import files added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./api/routes');
 
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', 3000); // aligned w/ Bitnami req - must open 3000 in EC2
 
 // Add middleware to console log every request
 app.use(function(req, res, next) {
